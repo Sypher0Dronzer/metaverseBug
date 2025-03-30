b0786f7f939877fde19246a19bb69cf9a2d13bb8
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-03-31 03:11:53 +0530

    updated stores

--- stores/BackgroundStore.ts
+++ stores/BackgroundStore.ts
@@ -1,6 +1,7 @@
 "use client";
 import { BackgroundMode } from "@/types/BackgroundMode";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { sanitizeId } from '../util'
 import { getGameInstance } from "../components/PhaserGame";
 import Preloader from "@/components/Preloader";
 const phaserGame = getGameInstance()
@@ -12,19 +13,35 @@ export function getInitialBackgroundMode() {
 export const backgroundSlice = createSlice({
     name: 'background',
     initialState: {
-        backgroundMode: getInitialBackgroundMode()
+        backgroundMode: getInitialBackgroundMode(),
+        sessionId: '',
+        loggedIn: false,
+        playerNameMap: new Map<string, string>(),
     },
     reducers: {
         toggleBackgroundMode: (state) => {
             const newMode = state.backgroundMode === BackgroundMode.DAY ? BackgroundMode.NIGHT : BackgroundMode.DAY
             state.backgroundMode = newMode
-            if (phaserGame){
+            if (phaserGame) {
                 const preloader = phaserGame.scene.keys.preloader as Preloader
                 preloader.changeBackgroundMode(newMode)
             }
         },
-    },})
+        setSessionId: (state, action: PayloadAction<string>) => {
+            state.sessionId = action.payload
+        },
+        setLoggedIn: (state, action: PayloadAction<boolean>) => {
+            state.loggedIn = action.payload
+        },
+        setPlayerNameMap: (state, action: PayloadAction<{ id: string; name: string }>) => {
+            state.playerNameMap.set(sanitizeId(action.payload.id), action.payload.name)
+        },
+        removePlayerNameMap: (state, action: PayloadAction<string>) => {
+            state.playerNameMap.delete(sanitizeId(action.payload))
+        },
+    },
+})
 
-export const { toggleBackgroundMode } = backgroundSlice.actions
+export const { toggleBackgroundMode,setLoggedIn,setSessionId,setPlayerNameMap,removePlayerNameMap } = backgroundSlice.actions
 export default backgroundSlice.reducer
 
