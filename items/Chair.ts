06f1df509d6f8aefc76d4325a732ea35882231e5
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-03-31 21:07:20 +0530

    added player functionalities (without network operations

--- items/Chair.ts
+++ items/Chair.ts
@@ -2,7 +2,7 @@ import { ItemType } from '../types/Items'
 import Item from './Item'
 
 export default class Chair extends Item {
-  itemDirection?: string
+  itemDirection?:  'up' | 'down' | 'left' | 'right';
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
     super(scene, x, y, texture, frame)
--- types/KeyboardState.ts
+++ types/KeyboardState.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
 
 export type Keyboard = {
-  W: Phaser.Input.Keyboard.Key
+  W: Phaser.Input.Keyboard.Key 
   S: Phaser.Input.Keyboard.Key
   A: Phaser.Input.Keyboard.Key
   D: Phaser.Input.Keyboard.Key
